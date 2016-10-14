5c5194fc1152178133566b2c083e28725ce8172a
Zade Kaakarni <xade.kaakarni@gmail.com>
2016-10-13 21:01:41 -0400

    Fixed creating user with Firebase. Removed logic from ToriiFirebaseAdapter

--- app/app/torii-adapters/application.js
+++ app/app/torii-adapters/application.js
@@ -1,22 +1,4 @@
-import Ember from 'ember';
 import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
 
 export default ToriiFirebaseAdapter.extend({
-  firebaseApp: Ember.inject.service(),
-  
-  actions: {
-
-    createUser(email, pass) {
-      const auth = this.get('firebaseApp').auth();
-      auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
-        const user = this.store.createRecord('user', {
-          id: userResponse.uid,
-          email: userResponse.email
-        });
-        return user.save();
-      });
-    }
-
-  }
-  
 });
